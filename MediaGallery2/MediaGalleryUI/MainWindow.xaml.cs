2bcd0112858eed9dfc940727474f75bd8583a816
mattias <mattias@62fe488c-53d3-405c-b4ae-af08ba54cbcf>
2010-10-14 19:54:48 +0000

    git-svn-id: svn://hubba/code/trunk@90 62fe488c-53d3-405c-b4ae-af08ba54cbcf

--- MediaGallery2/MediaGalleryUI/MainWindow.xaml.cs
+++ MediaGallery2/MediaGalleryUI/MainWindow.xaml.cs
@@ -14,14 +14,14 @@ using System.Windows.Shapes;
 
 namespace MediaGalleryUI
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+	}
 }
